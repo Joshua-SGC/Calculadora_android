70cb33b20befb93acdd70563b5cbd7209a899be3
JoshuaSGC <joshuagomez.canon@gmail.com>
2025-03-06 21:17:42 -0700

    Funciona?

--- app/src/main/java/mx/alumnos/uacj/dora_la_calculadora/MainActivity.kt
+++ app/src/main/java/mx/alumnos/uacj/dora_la_calculadora/MainActivity.kt
@@ -1,5 +1,8 @@
 package mx.alumnos.uacj.dora_la_calculadora
 
+import android.content.Context
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -7,7 +10,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -17,35 +22,45 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import mx.alumnos.uacj.dora_la_calculadora.ui.theme.Dora_la_calculadoraTheme
 
-data class BotonModelo(val id: String, var boton: String, var operacion_Aritmeticas: OperacionesAritmeticas = OperacionesAritmeticas.Ninguna) {
-    val numero: Any?
-}
+
+data class BotonModelo(
+    val id: String,
+    var numero: String,
+    var operacion_aritmetica: OperacionesAritmeticas = OperacionesAritmeticas.Ninguna,
+    var operacion_a_mostrar: String = "",
+) {}
 
 enum class EstadosCalculadora{
     CuandoEstaEnCero,
     AgregandoNumeros,
-    SeleccionandoOperacion,
+    SeleccionadoOperacion,
     MostrandoResultado
 }
 
 enum class OperacionesAritmeticas{
-    Ninguna, //Esta es la opcion por default
+    Ninguna, // Esta es la opcion por default y sirve para hacer nada
     Suma,
     Resta,
     Multiplicacion,
@@ -55,24 +70,24 @@ enum class OperacionesAritmeticas{
 
 var hileras_de_botones_a_dibujar = arrayOf(
     arrayOf(
-        BotonModelo("9","9", OperacionesAritmeticas.Multiplicacion),
-        BotonModelo("8","8"),
-        BotonModelo("7","7", OperacionesAritmeticas.Division),
+        BotonModelo("boton_9", "9", OperacionesAritmeticas.Multiplicacion, "*"),
+        BotonModelo("boton_8", "8"),
+        BotonModelo("boton_7", "7", OperacionesAritmeticas.Division, "/"),
     ),
     arrayOf(
-        BotonModelo("6","6"),
-        BotonModelo("5","5", OperacionesAritmeticas.Resultado),
-        BotonModelo("4","4"),
+        BotonModelo("boton_6", "6"),
+        BotonModelo("boton_5", "5", OperacionesAritmeticas.Resultado, "="),
+        BotonModelo("boton_4", "4"),
     ),
     arrayOf(
-        BotonModelo("3","3", OperacionesAritmeticas.Suma),
-        BotonModelo("2","2"),
-        BotonModelo("1","1", OperacionesAritmeticas.Resta),
+        BotonModelo("boton_3", "3", OperacionesAritmeticas.Suma, "+"),
+        BotonModelo("boton_2", "2"),
+        BotonModelo("boton_1", "1", OperacionesAritmeticas.Resta, "-"),
     ),
     arrayOf(
-        BotonModelo("btn_punto","btn_punto"),
-        BotonModelo("0","0"),
-        BotonModelo("btn_operacion","btn_operacion"),
+        BotonModelo("boton_punto", "."),
+        BotonModelo("boton_0", "0"),
+        BotonModelo("boton_operacion", "OP"),
     )
 
 )
@@ -83,132 +98,187 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Dora_la_calculadoraTheme {
-                    Calculadora()
-                }
+                Calculadora(modifier = Modifier.fillMaxSize(), contexto = this)
             }
         }
     }
+}
 
 @Composable
-fun Calculadora() {
+fun Calculadora(modifier: Modifier, contexto: Context? = null) {
     var pantalla_calculadora = remember { mutableStateOf("0") }
+    var numero_anterior = remember { mutableStateOf("0") }
     var estado_de_la_calculadora = remember { mutableStateOf(EstadosCalculadora.CuandoEstaEnCero) }
     var operacion_seleccionada = remember { mutableStateOf(OperacionesAritmeticas.Ninguna) }
 
     fun pulsar_boton(boton: BotonModelo){
-        Log.v("BOTONES-INTERFAZ", "se ha pulsado el boton ${boton.id} de la interfaz")
-        Log.v("OPERACION_SELECCIONADA", "la operacion seleccionada es ${operacion_seleccionada.value}")
+        Log.v("BOTONES_INTERFAZ", "Se ha pulsado el boton ${boton.id} de la interfaz")
+        Log.v("OPERACION_SELECCIONADA", "La operacion seleccionada es ${operacion_seleccionada.value}")
 
-        when(estado_de_la_calculadora.value) {
+        when(estado_de_la_calculadora.value){
             EstadosCalculadora.CuandoEstaEnCero -> {
-                if(boton.id == "boton_0") {
+                if(boton.id == "boton_0"){
                     return
-                }else if(boton.id == "boton_punto"){
+                }
+                else if(boton.id == "boton_punto"){
                     pantalla_calculadora.value = pantalla_calculadora.value + boton.numero
                     return
                 }
+
                 pantalla_calculadora.value = boton.numero
                 estado_de_la_calculadora.value = EstadosCalculadora.AgregandoNumeros
+
             }
 
             EstadosCalculadora.AgregandoNumeros -> {
                 if(boton.id == "boton_operacion"){
-                    estado_de_la_calculadora.value = EstadosCalculadora.SeleccionandoOperacion
+                    estado_de_la_calculadora.value = EstadosCalculadora.SeleccionadoOperacion
                     return
                 }
 
                 pantalla_calculadora.value = pantalla_calculadora.value + boton.numero
             }
 
-            EstadosCalculadora.SeleccionandoOperacion -> {
-                if(boton.operacion_aritmeticas != OperacionesAritmeticas.Ninguna){
-                    operacion_seleccionada.value = boton.operacion_Aritmeticas
+            EstadosCalculadora.SeleccionadoOperacion -> {
+                if(     boton.operacion_aritmetica != OperacionesAritmeticas.Ninguna &&
+                    boton.operacion_aritmetica != OperacionesAritmeticas.Resultado
+                ){
+                    operacion_seleccionada.value = boton.operacion_aritmetica
                     estado_de_la_calculadora.value = EstadosCalculadora.CuandoEstaEnCero
+
+                    numero_anterior.value = pantalla_calculadora.value
 
                     pantalla_calculadora.value = "0"
                     return
                 }
+                // Aqui imprimimos el resultado
+                else if(boton.operacion_aritmetica == OperacionesAritmeticas.Resultado &&
+                    operacion_seleccionada.value != OperacionesAritmeticas.Ninguna){
+
+                    when(operacion_seleccionada.value){
+
+                        OperacionesAritmeticas.Suma -> {
+                            pantalla_calculadora.value = (numero_anterior.value + "+" + pantalla_calculadora.value)
+                        }
+                        OperacionesAritmeticas.Resta -> {
+                            pantalla_calculadora.value = numero_anterior.value + "-" + pantalla_calculadora.value
+                        }
+                        OperacionesAritmeticas.Multiplicacion -> {
+                            pantalla_calculadora.value = numero_anterior.value + "*" + pantalla_calculadora.value
+                        }
+                        OperacionesAritmeticas.Division -> {
+                            pantalla_calculadora.value = numero_anterior.value + "/" + pantalla_calculadora.value
+                        }
+
+                        else -> {}
+                    }
+
+
+                    estado_de_la_calculadora.value = EstadosCalculadora.MostrandoResultado
+                    return
+                }
+
                 estado_de_la_calculadora.value = EstadosCalculadora.AgregandoNumeros
             }
 
-            EstadosCalculadora.MostrandoResultado -> TODO()
-        }
 
-        if(boton.id == "boton_operacion"){
-            estado_de_la_calculadora.value =
-        }
-        else{
-            pantalla_calculadora.value = boton.id
+            EstadosCalculadora.MostrandoResultado -> {
+                numero_anterior.value  = ""
+
+                pantalla_calculadora.value = "0"
+
+                estado_de_la_calculadora.value = EstadosCalculadora.CuandoEstaEnCero
+            }
         }
     }
-    Column(
-        modifier = Modifier.fillMaxSize(),
+
+
+    Column(modifier = modifier.fillMaxSize().background(Color.DarkGray),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Aqui van numeritos", modifier = Modifier
+        Text(text = "${pantalla_calculadora.value}", modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()
-            .fillMaxHeight()
-            .background(Color.DarkGray)
+            .fillMaxHeight(0.30f)
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color(0xFFBDBDBD).copy(alpha = 0.8f))
             .height(50.dp),
             textAlign = TextAlign.Right,
+            color = Color.Black,
+            fontSize = 56.sp
         )
 
-        /*Column(modifier = Modifier.fillMaxSize().background(Color.DarkGray)) {
-            for(fila_de_botones in hileras_de_botones_a_dibujar){
-                Row(horizontalArrangement = Arrangement.SpaceAround,
-                    modifier = Modifier.fillMaxWidth()){
-
-                        for(boton_a_dibujar in fila_de_botones){
-                            Boton(boton_a_dibujar.boton, alPulsar = {
-                                pulsar_boton(boton_a_dibujar)
-                            })}
-                }
-            }
-        }*/
+        // Deberia jugar mas con el estilo de aqui
         Column(modifier = Modifier
             .fillMaxSize()
             .background(Color.DarkGray)) {
-            for(boton_a_dibujar in fila_de_botones){
-                when(estado_de_la_calculadora.value){
-                    EstadosCalculadora.SeleccionandoOperacion
+            for(fila_de_botones in hileras_de_botones_a_dibujar){
+                Row(horizontalArrangement = Arrangement.SpaceAround,
+                    modifier = Modifier.fillMaxWidth()) {
+                    for(boton_a_dibujar in fila_de_botones){
+                        when(estado_de_la_calculadora.value){
+                            EstadosCalculadora.SeleccionadoOperacion -> {
+                                Boton(boton_a_dibujar.operacion_a_mostrar, alPulsar = {
+                                    pulsar_boton(boton_a_dibujar)
+                                })
+                            }
+                            else -> {
+                                Boton(boton_a_dibujar.numero, alPulsar = {
+                                    pulsar_boton(boton_a_dibujar)
+                                })
+                            }
+                        }
+
+                    }
                 }
             }
         }
     }
+
+
 }
 
 @Composable
-fun Boton(etiqueta: String, alPulsar: () -> Unit = {}) {
-    Button(onClick = alPulsar) {
-        Image(
-            painter = painterResource(R.drawable.images),
-            contentDescription = "Una foto de perfil del conde contar",
-            modifier = Modifier.size(25.dp)
-        )
+fun Boton(etiqueta: String, alPulsar: () -> Unit = {}, contexto: Context? = null){
+    //Diseño del boton
+    val fondoBoton = Color(0xFF2D2D2D)
+    val sombraColor = Color(0xFF1A1A1A)
 
-        Text(
-            etiqueta, modifier = Modifier
-                .background(Color.Green),
-            textAlign = TextAlign.Center,
-            color = Color.Red
-        )
+
+    Button(onClick = alPulsar, modifier = Modifier
+        .size(100.dp)
+        .clip(CircleShape)
+        .padding(5.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = fondoBoton),
+        shape = CircleShape,
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp)) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ){
+
+            Text(
+                etiqueta,
+                modifier = Modifier,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontSize = 24.sp
+            )
+        }
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Dora_la_calculadoraTheme {
-        Calculadora()
+        Calculadora(modifier = Modifier.fillMaxSize())
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun mostrar_boton() {
+fun mostrar_boton(){
     Dora_la_calculadoraTheme {
         Boton("4")
     }
